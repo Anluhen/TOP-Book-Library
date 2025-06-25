93ae567e40df2eda07f1d8bdbf26b0407cecad44
Henchenski <andrehenc@gmail.com>
2025-06-25 16:47:06 -0300

    feat(script.js): Populate table with objects

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function Book(title, author, pages, read) {
         throw Error("Must use the 'new' operator to create a instance");
     }
 
+    this.id = crypto.randomUUID()
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -21,5 +22,37 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+addBookToLibrary('1984', 'G. Orwell', 300, true);
 
-console.log(myLibrary[0].info()); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read"
+const table = document.getElementById('libraryTable');
+
+// Create table headers
+const thead = document.createElement('thead');
+const headerRow = document.createElement('tr');
+
+Object.keys(myLibrary[0]).forEach(key => {
+    const th = document.createElement('th');
+    console.log(key);
+    th.textContent = key
+    headerRow.appendChild(th);
+})
+
+thead.appendChild(headerRow);
+table.appendChild(thead);
+
+// Create table body and populate with books
+const tbody = document.createElement('tbody');
+
+myLibrary.forEach(book => {
+    const tr = document.createElement('tr')
+    
+    Object.values(book).forEach(value => {
+        const td = document.createElement('td');
+        td.textContent = value;
+        tr.appendChild(td);
+    });
+
+    tbody.appendChild(tr);
+});
+
+table.appendChild(tbody);
